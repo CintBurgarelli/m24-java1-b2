cb79738baf005046ad19e3191a6ae91cc8c2cc94
CintBurgarelli <cinthia12le@gmail.com>
2024-06-08 12:21:02 -0300

    Conteudo da aula 08-06-2024

---
+++ Eclipse/M24-Java1-B/src/Exercicio05Licao05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio05Licao05 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		Forca fo = new Forca();
+		fo.mostrarForca();
+		String palavra="";
+		while (!fo.isTerminado()) {
+			System.out.println("Digite uma letra: ");
+			palavra = sc.nextLine();
+			if (fo.verificaLetra(palavra.charAt(0))) {
+				System.out.println("Letra não encontrada!");
+			}
+			
+		}
+		sc.close();
+		System.out.println("A palavra secreta era " +
+		                          fo.mostrarPalavra());
+	}
+
+}
